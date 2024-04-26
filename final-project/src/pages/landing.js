90a862b52638dc83542cdcdcf11eb04bc42837fb
Brandon Chin <brandonzchin@gmail.com>
2024-04-25 22:53:02 -0400

    v0.10
    Login Frontend Complete. Form done. Need to connect Backend to supabase.

--- final-project/src/pages/landing.js
+++ final-project/src/pages/landing.js
@@ -4,7 +4,7 @@ import "./landing.css";
 const Landing = () => {
   return (
     <div>
-      <h1>Roogle</h1>
+      <h1 id="logo">Roogle</h1>
       <div className="login-container">
         <div className="login-title-container">
           <h2 id="title-sign-in">Sign In</h2>
@@ -12,14 +12,18 @@ const Landing = () => {
             Rate, Categorize, and Save Your Items Digitally
           </p>
         </div>
-        <input type="text" placeholder="Username" required></input>
-        <input type="password" placeholder="Password" required></input>
-        <a href="www" id="forget-button">
-          Forgot Password?
-        </a>
-        <button id="sign-in-button">Login</button>
-        <p>or</p>
-        <button>Register</button>s
+        <form className="form-container" method="post" autocomplete="off">
+          <input type="text" placeholder="Username" required></input>
+          <input type="password" placeholder="Password" required></input>
+          <a href="www" id="forget-button">
+            Forgot Password?
+          </a>
+          <button id="sign-in-button" type="submit">
+            Sign In
+          </button>
+          <p id="or-block">or</p>
+          <button id="register-button">Register</button>
+        </form>
       </div>
     </div>
   );
