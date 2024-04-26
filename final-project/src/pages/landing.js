8099de8e72d78141724f1edc0ab05910d9269272
Brandon Chin <brandonzchin@gmail.com>
2024-04-26 02:30:01 -0400

    v1.121

--- final-project/src/pages/landing.js
+++ final-project/src/pages/landing.js
@@ -2,13 +2,11 @@ import React from "react";
 import "./landing.css";
 import { createClient } from "@supabase/supabase-js";
 
-// Load environment variables
 const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.REACT_APP_ANNON_KEY;
-console.log("Supabase URL:", SUPABASE_URL);
-console.log("Supabase Anon Key:", SUPABASE_ANON_KEY);
+// console.log("Supabase URL:", SUPABASE_URL);
+// console.log("Supabase Anon Key:", SUPABASE_ANON_KEY);
 
-// Initialize Supabase client
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 const handleSignIn = async (event) => {
@@ -22,7 +20,6 @@ const handleSignIn = async (event) => {
     console.error("Error signing in:", error.message);
   } else {
     console.log("Signed in successfully:", user);
-    // Redirect or update UI accordingly
   }
 };
 
